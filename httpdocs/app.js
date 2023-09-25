5143c202560bf5f3f2ba696a1227bebf8bf57743
Sergey Gruzdov <sergey@gruzdov.dev>
2023-09-25 21:56:45 +0200

    yeeeee2

--- httpdocs/app.js
+++ httpdocs/app.js
@@ -3,26 +3,64 @@ document.addEventListener("DOMContentLoaded", function () {
     const addTaskButton = document.getElementById("addTask");
     const taskList = document.getElementById("taskList");
 
-    // Function to add a new task
+    // Function to fetch tasks from the server
+    function fetchTasks() {
+        fetch("/tasks")
+            .then((response) => response.json())
+            .then((data) => {
+                data.forEach((task) => {
+                    addTaskToList(task);
+                });
+            });
+    }
+
+    // Function to add a new task to the server and list
     function addTask() {
         const taskText = taskInput.value.trim();
         if (taskText !== "") {
-            const li = document.createElement("li");
-            li.innerHTML = `
-                <span>${taskText}</span>
-                <button class="delete">Delete</button>
-            `;
-            taskList.appendChild(li);
-            taskInput.value = "";
-            attachDeleteListener(li);
+            fetch("/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: `text=${encodeURIComponent(taskText)}`,
+            })
+                .then((response) => response.json())
+                .then((task) => {
+                    addTaskToList(task);
+                    taskInput.value = "";
+                });
         }
+    }
+
+    // Function to add a task to the list
+    function addTaskToList(task) {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <button class="delete" data-task-id="${task._id}">Delete</button>
+        `;
+        taskList.appendChild(li);
+        attachDeleteListener(li);
+    }
+
+    // Function to delete a task
+    function deleteTask(taskId) {
+        fetch(`/tasks/${taskId}`, { method: "DELETE" })
+            .then(() => {
+                const taskElement = document.querySelector(`[data-task-id="${taskId}"]`);
+                if (taskElement) {
+                    taskElement.parentElement.removeChild(taskElement);
+                }
+            });
     }
 
     // Function to attach delete listener to a task
     function attachDeleteListener(li) {
         const deleteButton = li.querySelector(".delete");
-        deleteButton.addEventListener("click", function () {
-            taskList.removeChild(li);
+        const taskId = deleteButton.getAttribute("data-task-id");
+        deleteButton.addEventListener("click", () => {
+            deleteTask(taskId);
         });
     }
 
@@ -35,4 +73,7 @@ document.addEventListener("DOMContentLoaded", function () {
             addTask();
         }
     });
+
+    // Fetch tasks when the page loads
+    fetchTasks();
 });
--- server.js
+++ server.js
@@ -1,11 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Create a MySQL connection
+const db = mysql.createConnection({
+    host: "localhost",       // Replace with your MySQL host
+    user: "sergeygruzdov",   // Replace with your MySQL username
+    password: "p7^643dUt",   // Replace with your MySQL password
+    database: "todoapp",     // Replace with your MySQL database name
+});
+
+// Connect to MySQL
+db.connect((err) => {
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+    } else {
+        console.log("Connected to MySQL");
+    }
+});
+
+// API endpoint to get tasks from the database
+app.get("/tasks", (req, res) => {
+    const sql = "SELECT * FROM tasks";
+    db.query(sql, (err, tasks) => {
+        if (err) {
+            console.error("Error fetching tasks:", err);
+            res.status(500).send("Error fetching tasks");
+        } else {
+            res.json(tasks);
+        }
+    });
+});
+
+// API endpoint to add a task to the database
+app.post("/tasks", (req, res) => {
+    const { text } = req.body;
+    const sql = "INSERT INTO tasks (text) VALUES (?)";
+    db.query(sql, [text], (err, result) => {
+        if (err) {
+            console.error("Error adding task:", err);
+            res.status(500).send("Error adding task");
+        } else {
+            res.json({ id: result.insertId, text });
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
